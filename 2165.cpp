22d1a47479435625c8bbceed66937f1454416bd6
Hasibull <hasibul.sbn@gmail.com>
2025-08-15 22:38:23 +0600

    Trying tower of hanoi problem

---
+++ 2165.cpp
@@ -0,0 +1,18 @@
+/**
+* Author: Hasibul Islam
+* Created on: 2025-08-15 21:46
+* Short Desc: The well known tower of Hanoi problem
+* IDEA:  
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    return 0;
+}
